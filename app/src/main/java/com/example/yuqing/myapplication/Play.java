1cfe50760977725a80001a46683b60b74231099f
Yuqing <happilynotorious@hotmail.com>
2016-02-06 15:17:17 -0800

    add ship

--- app/src/main/java/com/example/yuqing/myapplication/Play.java
+++ app/src/main/java/com/example/yuqing/myapplication/Play.java
@@ -6,14 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Play extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        Ship player = new Ship();
 
 
     }
